92ad2f11da96baaf6264c6747372e0b8f49a3641
Meta-Games <djrox316@gmail.com>
2018-02-09 18:47:45 -0500

    Created World Template
    To ease creation of new enviroments, includes:
    -teleporting
    -grabbing
    -menu with reset world button
    Removed:
    -Simulator
    -SDK switcher
    -cluttered Objects

--- Vive Test/Assets/WorldHandler.cs
+++ Vive Test/Assets/WorldHandler.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class WorldHandler : MonoBehaviour {
 
+	public Text text;
+	public bool check;
 	// Use this for initialization
 	void Start () {
-		
+		check = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	public void ReloadLinkWorld()
+	public void ReloadLinkWorld ()
 	{
-		SceneManager.LoadScene("LinkTestZone");
+		if (check) {
+			SceneManager.LoadScene ("LinkTestZone");
+		} else {
+			text.text = "Are you sure? You didnt just accidentally hit the button";
+		}
 	}
 }
